0a6b2d77734c7deaedfbab834c0e5ea86a344a6b
Afsana Shaikh <afsanashaikh2491@gmail.com>
2016-10-21 16:58:13 +0530

    first commit

---
+++ ASPattern1/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  ASPattern1
+//
+//  Created by Student P_02 on 21/10/16.
+//  Copyright © 2016 Afsana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+void pattern();
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        pattern();
+    }
+    return 0;
+}
+void pattern()
+{
+    int i ,j;
+    for(i=1;i<=5;i++)
+    {
+        for (j=1; j<=i;j++) {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+}
